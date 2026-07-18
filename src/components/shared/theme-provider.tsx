56dbb357cea896c6a477015213a6e23dd186793c
raphaelobene <raphaelobene@gmail.com>
2026-07-18 03:00:12 +0100

    chore(theme-provider): change source directory

---
+++ src/components/shared/theme-provider.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import * as React from "react"
+import { useHotkey } from "@tanstack/react-hotkeys"
+import { ThemeProvider as NextThemesProvider, useTheme } from "next-themes"
+
+import { HOTKEYS } from "@/lib/constants"
+import { isTypingTarget } from "@/lib/utils"
+
+import { Toaster } from "../ui/sonner"
+
+function ThemeProvider({
+	children,
+	...props
+}: React.ComponentProps<typeof NextThemesProvider>) {
+	return (
+		<NextThemesProvider
+			attribute="class"
+			defaultTheme="system"
+			enableSystem
+			disableTransitionOnChange
+			{...props}
+		>
+			<ThemeHotkey />
+			{children}
+			<Toaster position="top-right" visibleToasts={1} richColors />
+		</NextThemesProvider>
+	)
+}
+
+function ThemeHotkey() {
+	const { resolvedTheme, setTheme } = useTheme()
+
+	useHotkey(HOTKEYS.DARK_MODE, (e) => {
+		if (e.metaKey || e.ctrlKey || e.altKey) return
+		if (isTypingTarget(e.target)) return
+
+		setTheme(resolvedTheme === "dark" ? "light" : "dark")
+	})
+
+	return null
+}
+
+export { ThemeProvider }
