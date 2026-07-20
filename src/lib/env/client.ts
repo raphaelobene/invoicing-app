303baeb098e5a66b4c932d2f6ce87c9a049e9c6e
raphaelobene <raphaelobene@gmail.com>
2026-07-20 10:57:17 +0100

    feat: add client environment variable

--- src/lib/env/client.ts
+++ src/lib/env/client.ts
@@ -1,4 +1,5 @@
 import { createEnv } from "@t3-oss/env-nextjs"
+import z from "zod"
 
 /**
  * Client-side environment configuration
@@ -6,6 +7,10 @@ import { createEnv } from "@t3-oss/env-nextjs"
  * Must be prefixed with NEXT_PUBLIC_
  */
 export const env = createEnv({
-	client: {},
-	runtimeEnv: {},
+	client: {
+		NEXT_PUBLIC_APP_URL: z.url(),
+	},
+	runtimeEnv: {
+		NEXT_PUBLIC_APP_URL: process.env["NEXT_PUBLIC_APP_URL"],
+	},
 })
